3c92385e0fb7c1746058be84334b68026e832378
Victor Asmad <vasmad@bdtglobal.com>
2025-03-19 15:30:14 -0300

    FEAT - Inicio de agregado de libreria de graficos

--- DataStructures/graphStructure/graphStructure.go
+++ DataStructures/graphStructure/graphStructure.go
@@ -1,5 +1,16 @@
 package graphStructure
 
+import (
+	"fmt"
+	"gonum.org/v1/gonum/graph/simple"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+	"image/color"
+	"math"
+)
+
 type Graph struct {
 	Vertices map[int]*Vertex
 }
@@ -66,4 +77,46 @@ func WithAdjacencyList(list map[int][]int) GraphOption {
 			}
 		}
 	}
+}
+
+func (g *Graph) Draw(filename string) error {
+	p := plot.New()
+	p.Title.Text = "Grafo"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+
+	pts := make(plotter.XYs, len(g.Vertices))
+	nodePositions := make(map[int]plotter.XY)
+
+	i := 0
+	for vertex := range g.Vertices {
+		angle := 2 * math.Pi * float64(i) / float64(len(g.Vertices))
+		pts[i].X = math.Cos(angle)
+		pts[i].Y = math.Sin(angle)
+		nodePositions[vertex] = pts[i]
+		i++
+	}
+
+	scatter, err := plotter.NewScatter(pts)
+	if err != nil {
+		return err
+	}
+
+	scatter.GlyphStyle.Color = color.RGBA{R: 255, A: 255}
+	p.Add(scatter)
+
+	lines := make([]plotter.XYs, 0)
+	for u := range g.Vertices {
+		for _, v := range g.Neighbors(u) {
+			lines = append(lines, plotter.XYs{
+				nodePositions[g.Vertices[u].Val],
+				nodePositions[g.Vertices[v].Val],
+			})
+		}
+	}
+
+	for _, line := range lines {
+		
+	}
+
 }
